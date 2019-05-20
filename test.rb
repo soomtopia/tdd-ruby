758e6d175b417797928f2f72bd23eb834429fbcf
soomtopia <soomtopia@gmail.com>
2019-05-20 14:58:39 +0900

    c1.43 테스트 생성하기

---
+++ test.rb
@@ -0,0 +1,10 @@
+# example
+require 'test/unit' 
+
+class DollarTests < Test::Unit::TestCase
+  def test_multiplication()
+    five = Dollar.new(5)
+    five.times(2)
+    assert_equal(10, five.amount)
+  end
+end
