ac7b37e5ce60a9f4af5b0f33f91cbd0c9e0c8d09
soomtopia <soomtopia@gmail.com>
2019-05-27 19:08:16 +0900

    c2.55 테스트 작성

--- test.rb
+++ test.rb
@@ -6,6 +6,8 @@ class DollarTests < Test::Unit::TestCase
   def test_multiplication()
     five = Dollar.new(5)
     five.times(2)
-    assert_equal(10, five.amount)
+    assert_equal(10, product.amount)
+    five.times(3)
+    assert_equal(15, product.amount)
   end
 end
