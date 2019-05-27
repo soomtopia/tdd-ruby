e04d8317a38621b2a14ec2d2bb2aff65c6773aec
soomtopia <soomtopia@gmail.com>
2019-05-27 19:10:10 +0900

    c2.55 테스트 통과할 방법이 없음. 새로운 객체반환으로 해결해보자

--- test.rb
+++ test.rb
@@ -5,8 +5,10 @@ require './dollar'
 class DollarTests < Test::Unit::TestCase
   def test_multiplication()
     five = Dollar.new(5)
+    product = five.times(2)
     five.times(2)
     assert_equal(10, product.amount)
+    product = five.times(3)
     five.times(3)
     assert_equal(15, product.amount)
   end
