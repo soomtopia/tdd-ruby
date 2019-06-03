0ce29c435c3d69cebeabd3fbc6ae5e32ea81f71e
soomtopia <soomtopia@gmail.com>
2019-06-03 15:11:35 +0900

    c5.70 test_franc 작성

--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 require './dollar'
 
 class DollarTests < Test::Unit::TestCase
-  def test_multiplication()
+  def test_multiplication
     five = Dollar.new(5)
     assert_equal(Dollar.new(10).amount, five.times(2).amount)
     assert_equal(Dollar.new(15).amount, five.times(3).amount)
@@ -12,5 +12,11 @@ class DollarTests < Test::Unit::TestCase
   def test_equality
     assert_true(Dollar.new(5) == Dollar.new(5))
     assert_false(Dollar.new(5) == Dollar.new(6))
+  end
+
+  def test_franc_multiplication
+    five = Franc.new(5)
+    assert_equal(Franc.new(10).amount, five.times(2).amount)
+    assert_equal(Franc.new(15).amount, five.times(3).amount)
   end
 end
