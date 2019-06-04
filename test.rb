19ddf4b0a297fcd8638730c1bf3e666ca7a50c7e
soomtopia <soomtopia@gmail.com>
2019-06-04 14:14:56 +0900

    c6.79 franc equality 테스트코드 추가

--- test.rb
+++ test.rb
@@ -12,6 +12,8 @@ class DollarTests < Test::Unit::TestCase
   def test_equality
     assert_true(Dollar.new(5) == Dollar.new(5))
     assert_false(Dollar.new(5) == Dollar.new(6))
+    assert_true(Franc.new(5) == Franc.new(5))
+    assert_false(Franc.new(5) == Franc.new(6))
   end
 
   def test_franc_multiplication
