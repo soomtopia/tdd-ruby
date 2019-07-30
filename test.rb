24727815410f1b12874615e06730837259140b86
soomtopia <soomtopia@gmail.com>
2019-07-30 18:37:50 +0900

    c8.90 팩토리메서드를 사용한 상속구조 변경

--- test.rb
+++ test.rb
@@ -6,16 +6,16 @@ require './franc'
 class DollarTests < Test::Unit::TestCase
   def test_multiplication
     five = Money.dollar(5)
-    assert_equal(Dollar.new(10).amount, five.times(2).amount)
-    assert_equal(Dollar.new(15).amount, five.times(3).amount)
+    assert_equal(Money.dollar(10).amount, five.times(2).amount)
+    assert_equal(Money.dollar(15).amount, five.times(3).amount)
   end
 
   def test_equality
-    assert_true(Dollar.new(5) == Dollar.new(5))
-    assert_false(Dollar.new(5) == Dollar.new(6))
+    assert_true(Money.dollar(5) == Money.dollar(5))
+    assert_false(Money.dollar(5) == Money.dollar(6))
     assert_true(Franc.new(5) == Franc.new(5))
     assert_false(Franc.new(5) == Franc.new(6))
-    assert_false(Franc.new(5) == Dollar.new(5))
+    assert_false(Franc.new(5) == Money.dollar(5))
   end
 
   def test_franc_multiplication
