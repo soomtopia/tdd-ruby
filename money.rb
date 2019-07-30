88910be76ed037c9cad127ab68ac41644d0cf82b
soomtopia <soomtopia@gmail.com>
2019-07-30 18:35:13 +0900

    c8.89 dollar 변경

--- money.rb
+++ money.rb
@@ -1,8 +1,13 @@
+require './dollar'
 class Money
   attr_accessor :amount
 
   def ==(other)
     money = other
     (amount == money.amount) && (self.class == money.class)
+  end
+
+  def self.dollar(amount)
+    Dollar.new(amount)
   end
 end
