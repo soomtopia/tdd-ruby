08fa64c19df2ae3eb5165bfd5f64cd1672dff353
soomtopia <soomtopia@gmail.com>
2019-07-30 18:39:15 +0900

    c8.91 franc 구조 변경

--- test.rb
+++ test.rb
@@ -13,14 +13,14 @@ class DollarTests < Test::Unit::TestCase
   def test_equality
     assert_true(Money.dollar(5) == Money.dollar(5))
     assert_false(Money.dollar(5) == Money.dollar(6))
-    assert_true(Franc.new(5) == Franc.new(5))
-    assert_false(Franc.new(5) == Franc.new(6))
-    assert_false(Franc.new(5) == Money.dollar(5))
+    assert_true(Money.franc(5) == Money.franc(5))
+    assert_false(Money.franc(5) == Franc.new(6))
+    assert_false(Money.franc(5) == Money.dollar(5))
   end
 
   def test_franc_multiplication
-    five = Franc.new(5)
-    assert_equal(Franc.new(10).amount, five.times(2).amount)
-    assert_equal(Franc.new(15).amount, five.times(3).amount)
+    five = Money.franc(5)
+    assert_equal(Money.franc(10).amount, five.times(2).amount)
+    assert_equal(Money.franc(15).amount, five.times(3).amount)
   end
 end
