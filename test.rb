ae90b1e7be7aec2939ade2742512c077dad4557d
soomtopia <soomtopia@gmail.com>
2019-08-02 11:51:24 +0900

    ch11.111 불필요 테스트 제거

--- test.rb
+++ test.rb
@@ -13,8 +13,6 @@ class DollarTests < Test::Unit::TestCase
   def test_equality
     assert_true(Money.dollar(5) == Money.dollar(5))
     assert_false(Money.dollar(5) == Money.dollar(6))
-    assert_true(Money.franc(5) == Money.franc(5))
-    assert_false(Money.franc(5) == Money.franc(6))
     assert_false(Money.franc(5) == Money.dollar(5))
   end
 
@@ -27,9 +25,5 @@ class DollarTests < Test::Unit::TestCase
   def test_currency
     assert_equal(:USD, Money.dollar(1).currency)
     assert_equal(:CHF, Money.franc(1).currency)
-  end
-
-  def test_differnt_class_equality
-    assert_true(Money.new(10, :CHF) == Franc.new(10, :CHF))
   end
 end
