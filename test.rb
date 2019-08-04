a7338ca49dda657e727e383674589a637e1c6839
soomtopia <soomtopia@gmail.com>
2019-08-04 14:13:56 +0900

    c13.124 통화 추가

--- test.rb
+++ test.rb
@@ -43,4 +43,10 @@ class DollarTests < Test::Unit::TestCase
     assert_equal(five, sum.addend)
   end
 
+  def test_reduce_sum
+    sum = Sum.new(Money.dollar(3), Money.dollar(4))
+    bank = Bank.new()
+    result = bank.reduce(sum, :USD)
+    assert_equal(Money.dollar(7), result)
+  end
 end
